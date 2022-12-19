066efb9a34609134d2880b342ea02bd1c7125898
nikhilkc96 <nikhilkc96@gmail.com>
2022-12-19 16:07:10 +0100

    added honeyd install

--- init.sh
+++ init.sh
@@ -17,7 +17,34 @@ sleep 3
 sudo apt update
 
 
- sysctl -w vm.max_map_count=262144
+# Installing necessary packages
+sudo apt install -y libevent-dev libdumbnet-dev libpcap-dev libpcre3-dev libedit-dev bison flex libtool automake python3-pip python3
+
+# Install farpd
+sudo apt install -y farpd
+
+# Get python2 pip
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+sudo python2 get-pip.py
+rm get-pip.py
+
+# CPPPO Correct Version 4.3.4
+sudo pip2 install cpppo==4.3.4
+
+# Installing Honeyd - just a copy and paste of the guide
+cd ~
+git clone https://github.com/DataSoft/Honeyd
+cd Honeyd
+sudo apt install -y libevent-dev libdumbnet-dev libpcap-dev libpcre3-dev libedit-dev bison flex libtool automake make zlib1g-dev
+./autogen.sh
+./configure
+make
+sudo make install
+
+
+
+
+sysctl -w vm.max_map_count=262144
  
 # RUN Bottle Factory 
 docker-compose -f bottlefactory/docker-compose.yml up -d
