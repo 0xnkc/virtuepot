0096e1b440e71e3b097a94eb13f4f45d0c3bf0bd
Nikhil K C <nikhilkc96@gmail.com>
2023-07-09 15:17:07 +0200

    zeek in init.sh

--- README.md
+++ README.md
@@ -15,14 +15,14 @@ The above-mentioned diagram shows the draft copy of the architecture
 To install Docker Engine, you need the 64-bit version of one of these Ubuntu versions:
 
 * Ubuntu Jammy 22.04 (LTS)
-* Ubuntu Impish 21.10
 * Ubuntu Focal 20.04 (LTS)
 * Ubuntu Bionic 18.04 (LTS)
 
 Docker Engine is compatible with x86_64 (or amd64), armhf, arm64, and s390x architectures.
 
 * [Install Docker](./doc/docker.md)
-* [Installation of Arkime (Monitoring Node)](./doc/arkime.md)
+* [Installation of Zeek (Monitoring Node)](./doc/zeek.md)
 * [Installation of OpenPLC (Running Water Tank)](./doc/openplc.md)
 * [Installation of ICSSIM (Running Bottle Factory)](./doc/icssim.md)
 * [Installation of Scada ](./doc/scada.md)
+* [Installation of HoneyD ](./doc/honeyd.md)
--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ version=$(lsb_release -rs )
 # Wrong version warning
 if [ "$version" != "22.04" ] && [ "$version" != "20.04" ] && [ "$version" != "18.04" ];
 then
-  printf "Warning! This installation script has only been tested on Ubuntu 20.04 LTS and 18.04 LTS and will likely not work on your Ubuntu version.\n\n"
+  printf "Warning! This installation script has only been tested on Ubuntu 20.04, 22.04 LTS and 18.04 LTS and will likely not work on your Ubuntu version.\n\n"
 fi
 
 sleep 3
@@ -17,36 +17,7 @@ sleep 3
 sudo apt update
 
 
-# # Installing necessary packages
-# sudo apt install -y libevent-dev libdumbnet-dev libpcap-dev libpcre3-dev libedit-dev bison flex libtool automake python3-pip python3
-
-# # Install farpd
-# sudo apt install -y farpd
-
-# # Get python2 pip
-# curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
-# sudo python2 get-pip.py
-# rm get-pip.py
-
-# # CPPPO Correct Version 4.3.4
-# sudo pip2 install cpppo==4.3.4
-
-# # Installing Honeyd - just a copy and paste of the guide
-# cd ~
-# git clone https://github.com/DataSoft/Honeyd
-# cd Honeyd
-# sudo apt install -y libevent-dev libdumbnet-dev libpcap-dev libpcre3-dev libedit-dev bison flex libtool automake make zlib1g-dev
-# ./autogen.sh
-# ./configure
-# make
-# sudo make install
-
-
-
-
 sysctl -w vm.max_map_count=262144
-
-
 
  
 # RUN Bottle Factory 
@@ -60,11 +31,11 @@ docker-compose -f watertank/docker-compose.yml up -d
 docker-compose -f scada/docker-compose.yml up -d
 
 
-# RUN arkime
-docker-compose -f arkime/docker-compose.yml up -d
+#Run the zeek
+docker-compose -f zeek/docker-compose.yml up -d
+
 
 #Run the honeyd
-
 docker-compose -f honeyd/docker-compose.yml up -d
 
 
