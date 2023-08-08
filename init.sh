88990bd07b1257e953a6ece5ec930a81dd7a72ae
Nikhil K C <nikhilkc96@gmail.com>
2023-08-08 12:59:08 +0200

    added docker installation steps

--- init.sh
+++ init.sh
@@ -1,5 +1,3 @@
-
-
 #!/bin/bash
 
 cwd=$(pwd)
@@ -11,15 +9,57 @@ then
   printf "Warning! This installation script has only been tested on Ubuntu 20.04, 22.04 LTS and 18.04 LTS and will likely not work on your Ubuntu version.\n\n"
 fi
 
+#install  docker and docker-compose
+echo "Install Docker on your Ubuntu system."
+read -p "Do you want to proceed with the installation? (y/n): " choice
+
+if [ "$choice" != "y" ]; then
+    echo "Installation aborted."
+    exit 1
+fi
+
+echo "Updating package index..."
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+echo "Installing dependencies..."
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+echo "Adding Docker's official GPG key..."
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "Adding Docker repository..."
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
+
+echo "Updating package index again..."
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt-get update
+
+echo "Installing Docker..."
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
+
+echo "Docker installation completed!"
+#FOr permission denied while trying to connect to the Docker daemon socket
+sudo chmod 666 /var/run/docker.sock
+
+
 sleep 3
 
 # Update apt
 sudo apt update
-
-
+#Disable the firewall
+sudo ufw disable 
+#set the vm.max_map_count for elasticsearch
 sysctl -w vm.max_map_count=262144
 
- 
 # RUN Bottle Factory 
 docker-compose -f bottlefactory/docker-compose.yml up -d
 
